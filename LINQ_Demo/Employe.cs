8e7484efaf8b241179b222181c6de4ccabd6b592
Zain <zainofficial03@gmail.com>
2021-05-21 17:02:16 +0500

    Add project files.

---
+++ LINQ_Demo/Employe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ_Demo
+{
+    class Employe
+    {
+        public int Empno { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+        public string Gender { get; set; }
+        public int Salary { get; set; }
+    }
+}
---
+++ LINQ_Demo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+namespace LINQ_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employe> employeList = new List<Employe>()
+           {
+               new Employe{Empno=1,Name="Meraj",Age=14,Country="Pakistan",Gender="male",Salary=1200},
+               new Employe{Empno=2,Name="Molai",Age=24,Country="India",Gender="male",Salary=100},
+               new Employe{Empno=3,Name="Maroof",Age=44,Country="China",Gender="male",Salary=12000},
+               new Employe{Empno=4,Name="Haseeb",Age=44,Country="China",Gender="male",Salary=2000},
+               new Employe{Empno=5,Name="Bushra",Age=44,Country="China",Gender="female",Salary=1000},
+           };
+
+            var result1 = from em1 in employeList select em1;
+
+            Console.WriteLine("Result Is ");
+
+            foreach(var v  in result1)
+            {
+                Console.WriteLine("Employe No : "+v.Empno+"\nEmploye Name : "+v.Name+"\nEmploye Age : "+v.Age+"\nEmploye Country : "+v.Country+"\nEmploye Gender : "+v.Gender+"\nEmploye Salary : "+v.Salary);
+                Console.WriteLine("*********************************");
+            }
+
+            var result2 = from em2 in employeList
+                          select new
+                          {
+                              em2.Empno,
+                              em2.Name,
+                              em2.Country
+                          };
+            foreach(var v in result2)
+            {
+                Console.WriteLine("Employe No : "+v.Empno+"\nEmploye Name : "+v.Name+"\nEmploye Country : "+v.Country);
+
+                Console.WriteLine("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+            }
+                    
+            var result3 =  from em3 in employeList where em3.Country.Equals("China") select em3;
+            Console.WriteLine("China Records");
+            foreach(var v in result3)
+            {
+                Console.WriteLine("Employe No : " + v.Empno + "\nEmploye Name : " + v.Name + "\nEmploye Age : " + v.Age + "\nEmploye Country : " + v.Country + "\nEmploye Gender : " + v.Gender + "\nEmploye Salary : " + v.Salary);
+                Console.WriteLine("///////////////////////////////////////////");
+            }
+
+            var result4 = from em in employeList orderby em.Salary select em;
+            Console.WriteLine("Sorted Records......");
+            foreach(var v in result4)
+            {
+                Console.WriteLine("Employe No : " + v.Empno + "\nEmploye Name : " + v.Name + "\nEmploye Age : " + v.Age + "\nEmploye Country : " + v.Country + "\nEmploye Gender : " + v.Gender + "\nEmploye Salary : " + v.Salary);
+                Console.WriteLine("*********$$$$$$$$$$$$$$$$********************");
+            }
+            
+        }
+    }
+}
